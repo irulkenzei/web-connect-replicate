fd7f83af9dfc50419ee8f05df1751eb300d77602
Chairul Anwar <chairulanwar2104@gmail.com>
2026-07-07 15:09:48 +0700

    Refactor main.js to index.js with voice generation
    Refactor main.js to index.js, integrating Replicate API for voice generation and supporting multiple languages and formats.

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { getStaticFile } from './utils.js';
-
-export default async ({ req, res, error }) => {
-  if (req.method === 'GET') {
-    return res.text(getStaticFile('index.html'), 200, {
-      'Content-Type': 'text/html; charset=utf-8',
-    });
-  }
-};
